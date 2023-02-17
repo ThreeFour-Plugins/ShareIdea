eebdf623f088bfa9c0d177fe0099d1e9d541533d
aminegames125 <aminegames125@gmail.com>
2023-02-17 13:35:14 +0000

    ShareIdea v1.0-SNAPSHOT

--- src/main/java/me/amineos/shareidea/ShareIdea.java
+++ src/main/java/me/amineos/shareidea/ShareIdea.java
@@ -5,6 +5,7 @@ import me.amineos.shareidea.commands.HelpCommand;
 import me.amineos.shareidea.commands.example;
 import me.amineos.shareidea.commands.reload;
 import me.amineos.shareidea.events.WelcomeMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
@@ -22,6 +23,7 @@ public final class ShareIdea extends JavaPlugin {
         this.getCommand("reload").setExecutor(new reload(this));
         getServer().getPluginManager().registerEvents(new WelcomeMessage(this),this);
         getCommand("help").setExecutor(new HelpCommand(this));
+        this.saveDefaultConfig();
     }
 
     @Override
